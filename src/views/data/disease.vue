49495bf6de614ea92724fd387525bf6af4df52e5
zhengyanzi <zhengyanzi@tiaohuo.com>
2020-04-22 22:17:11 +0800

    二级病种数据交互

--- src/views/data/disease.vue
+++ src/views/data/disease.vue
@@ -14,10 +14,10 @@
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="{row,$index}">
-          <el-button type="primary" icon="el-icon-edit" @click="handleUpdate(row)">
+          <el-button type="primary" icon="el-icon-edit" @click="handleUpdate(row,'one')">
             编辑
           </el-button>
-          <el-button icon="el-icon-delete" type="danger" @click="handleDelete(row,$index)">
+          <el-button icon="el-icon-delete" type="danger" @click="handleDelete(row,$index,'one')">
             删除
           </el-button>
         </template>
@@ -45,10 +45,10 @@
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="{row,$index}">
-          <el-button type="primary" icon="el-icon-edit" @click="handleUpdate(row)">
+          <el-button type="primary" icon="el-icon-edit" @click="handleUpdate(row,'two')">
             编辑
           </el-button>
-          <el-button icon="el-icon-delete" type="danger" @click="handleDelete(row,$index)">
+          <el-button icon="el-icon-delete" type="danger" @click="handleDelete(row,$index,'two')">
             删除
           </el-button>
         </template>
@@ -59,8 +59,8 @@
 
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
       <el-form ref="dataForm" :rules="rules" :model="temp" label-position="right" label-width="100px" style="width: 400px; margin-left:50px;">
-        <el-form-item label="名称" prop="first_level" v-if="createLevel == 'two' || updateLevel == 'two'">
-          <el-select v-model="temp.first_level" class="filter-item" placeholder="请选择一级病种" @change="changeDisease('filter',$event)">
+        <el-form-item label="名称" prop="first_level_name" v-if="createLevel == 'two' || updateLevel == 'two'">
+          <el-select v-model="temp.first_level_name" class="filter-item" placeholder="请选择一级病种" @change="changeDisease($event)">
             <el-option v-for="item in list" :key="item.id" :label="item.name" :value="item.id"/>
           </el-select>
         </el-form-item>
@@ -77,6 +77,31 @@
         </el-button>
       </div>
     </el-dialog>
+
+
+
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogLevelVisible">
+      <el-form ref="levelForm" :rules="rules" :model="updateLevelTemp" label-position="right" label-width="100px" style="width: 400px; margin-left:50px;">
+        <el-form-item label="名称" prop="first_level_name">
+          <el-select v-model="updateLevelTemp.first_level_name" class="filter-item" placeholder="请选择一级病种" @change="changeDisease($event)">
+            <el-option v-for="item in list" :key="item.id" :label="item.name" :value="item.id"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="名称11" prop="second_level_name">
+          <el-input v-model="updateLevelTemp.second_level_name" placeholder="请输入病种名称" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogLevelVisible = false">
+          取消
+        </el-button>
+        <el-button type="primary" @click="dialogStatus==='create'?createData():updateData(updateId)">
+          确认
+        </el-button>
+      </div>
+    </el-dialog>
+
+
   </div>
 </template>
 
@@ -114,18 +139,26 @@
         levelListLoading: true,
         listLoading: true,
         temp: {
-          first_level:'',
+          first_level_name:'',
           first_level_id:undefined,
           name:''
+        },
+        updateLevelTemp: {
+          first_level_name:'',
+          first_level_id:undefined,
+          name:'',
+          second_level_name:''
         },
         textMap: {
           update: '编辑门诊病种信息',
           create: '新增门诊病种信息'
         },
         dialogFormVisible: false,
+        dialogLevelVisible:false,
         dialogStatus: '',
         rules: {
           name:[{ required: true, message: '请输入病种名称', trigger: 'change' }],
+          second_level_name:[{ required: true, message: '请输入病种名称', trigger: 'change' }],
         },
         loading: false,
         updateId:undefined,
@@ -139,7 +172,7 @@
       this.getLevelList();
     },
     methods: {
-      changeDisease(val,e){
+      changeDisease(e){
         this.temp.first_level_id = e;
       },
       getList() {
@@ -160,7 +193,7 @@
       },
       resetTemp() {
         this.temp = {
-          first_level:'',
+          first_level_name:'',
           first_level_id:undefined,
           name:''
         }
@@ -180,7 +213,7 @@
             if(this.createLevel == 'one'){
               let params = Object.assign({},this.temp);
               this.$delete(params,'first_level_id');
-              this.$delete(params,'first_level');
+              this.$delete(params,'first_level_name');
               diseaseAdd(params).then((res) => {
                 this.list.unshift(res.data);
                 this.dialogFormVisible = false;
@@ -191,64 +224,118 @@
               })
             }else if(this.createLevel=='two'){
               let params = Object.assign({},this.temp);
-              this.$delete(params,'first_level');
-              console.log(params)
-              // levelDiseaseAdd(params).then((res) => {
-              //   this.levelList.unshift(res.data);
-              //   this.dialogFormVisible = false;
-              //   this.$message({
-              //     message: '增加成功',
-              //     type: 'success'
-              //   });
-              // })
+              this.$delete(params,'first_level_name');
+              levelDiseaseAdd(params).then((res) => {
+                this.levelList.unshift(res.data);
+                this.dialogFormVisible = false;
+                this.$message({
+                  message: '增加成功',
+                  type: 'success'
+                });
+              })
             }
 
           }
         })
       },
-      handleUpdate(row) {
-        this.temp = Object.assign({}, row); // copy obj
+      handleUpdate(row,name) {
         this.updateId = row.id;
+        this.updateLevel = name;
+        if(name=='one'){
+          this.temp = Object.assign({}, row); // copy obj
+          this.dialogFormVisible = true;
+          this.$nextTick(() => {
+            this.$refs['dataForm'].clearValidate()
+          })
+        }else if(name=='two'){
+          this.dialogLevelVisible = true;
+          this.updateLevelTemp = Object.assign({}, row); // copy obj
+          console.log(this.updateLevelTemp)
+          this.$nextTick(() => {
+            this.$refs['levelForm'].clearValidate()
+          })
+        }
         this.dialogStatus = 'update';
-        this.dialogFormVisible = true;
-        this.$nextTick(() => {
-          this.$refs['dataForm'].clearValidate()
-        })
+
       },
       updateData(id) {
-        this.$refs['dataForm'].validate((valid) => {
-          if (valid) {
-            const tempData = Object.assign({}, this.temp);
-            diseaseUpdate(id,tempData).then((res) => {
-              const index = this.list.findIndex(v => v.id === this.temp.id);
-              this.list.splice(index, 1, res.data);
-              this.dialogFormVisible = false;
-              this.$message({
-                message: '修改成功',
-                type: 'success'
-              });
-            })
-          }
-        })
+        if(this.updateLevel == 'one'){
+          this.$refs['dataForm'].validate((valid) => {
+            if (valid) {
+              const tempData = Object.assign({}, this.temp);
+              diseaseUpdate(id,tempData).then((res) => {
+                const index = this.list.findIndex(v => v.id === this.temp.id);
+                this.list.splice(index, 1, res.data);
+                this.dialogFormVisible = false;
+                this.$message({
+                  message: '修改成功',
+                  type: 'success'
+                });
+              })
+            }
+          })
+        }else if(this.updateLevel == 'two'){
+          this.$refs['levelForm'].validate((valid) => {
+            if (valid) {
+              const tempData = Object.assign({}, this.updateLevelTemp);
+              this.$delete(tempData,'first_level_id');
+              this.$delete(tempData,'first_level_name');
+              this.$delete(tempData,'id');
+              this.$delete(tempData,'second_level_name');
+              // tempData.name =  tempData.second_level_name;
+              this.$set(tempData,'name',this.updateLevelTemp.second_level_name);
+              console.log(tempData)
+              levelDiseaseUpdate(tempData.second_level_id,tempData).then((res) => {
+                const index = this.levelList.findIndex(v => v.second_level_id === this.updateLevelTemp.second_level_id);
+                this.levelList.splice(index, 1, res.data);
+                this.dialogLevelVisible = false;
+                this.$message({
+                  message: '修改成功',
+                  type: 'success'
+                });
+              })
+
+
+            }
+          })
+        }
+
       },
-      handleDelete(row, index) {
+      handleDelete(row, index,name) {
             this.$confirm('确定删除此记录吗?', '提示', {
               type: 'warning'
             }).then(() => {
-              this.listLoading = true;
+
               //NProgress.start();
               let para = {id: row.id};
-              diseaseDel(row.id).then((res) => {
-                this.listLoading = false;
-                if(res.status == 0) {
-                  this.list.splice(index, 1);
-                  //NProgress.done();
-                  this.$message({
-                    message: '删除成功',
-                    type: 'success'
-                  });
-                }
-              });
+              if(name == 'one'){
+                this.listLoading = true;
+                diseaseDel(row.id).then((res) => {
+                  this.listLoading = false;
+                  if(res.status == 0) {
+                    this.list.splice(index, 1);
+                    //NProgress.done();
+                    this.$message({
+                      message: '删除成功',
+                      type: 'success'
+                    });
+                  }
+                });
+              }else if(name == 'two'){
+                this.levelListLoading = true;
+                levelDiseaseDel(row.second_level_id).then((res) => {
+                  this.levelListLoading = false;
+                  if(res.status == 0) {
+                    this.levelList.splice(index, 1);
+                    //NProgress.done();
+                    this.$message({
+                      message: '删除成功',
+                      type: 'success'
+                    });
+                  }
+                });
+              }
+
             }).catch(() => {
 
             });
